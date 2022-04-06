4fbde53b4b382a2f4abcfb9200142207ebe4e834
ItayElf <thh123321@gmail.com>
2022-04-06 12:14:06 +0300

    fixed options modal rendering empty elements

--- src/components/modals/optionsModal.tsx
+++ src/components/modals/optionsModal.tsx
@@ -10,12 +10,14 @@ const OptionsModal: React.FC<Props> = ({ isOpen, onClose, children }) => {
   return (
     <ModalWrapper isOpen={isOpen} onClose={onClose} className="mx-4 w-96">
       <div className="flex w-full flex-col items-center justify-center space-y-2 text-center">
-        {React.Children.map(children, (child, i) => (
-          <>
-            {i !== 0 && <div className="h-[2px] w-full bg-primary-600" />}
-            {child}
-          </>
-        ))}
+        {React.Children.toArray(children)
+          .filter((x) => !!x)
+          .map((child, i) => (
+            <div key={i} className="w-full">
+              {i !== 0 && <div className="h-[2px] w-full bg-primary-600" />}
+              {child}
+            </div>
+          ))}
       </div>
     </ModalWrapper>
   );
